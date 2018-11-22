c16c53b0ba6f74659619ecc64dd039e7a4fecd27
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-11-23 01:06:30 +0530

    Finished linear regression with a single variable.

--- Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
@@ -1,9 +1,17 @@
 # Data Preprocessing Template
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = pd.read.csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
