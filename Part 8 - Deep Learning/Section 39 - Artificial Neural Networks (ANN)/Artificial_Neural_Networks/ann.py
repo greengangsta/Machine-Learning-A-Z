c2d08cc00f7596608951c5402e292824699490e0
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-11-29 13:07:19 +0530

    churn modelling
    Implemented Churn machine learning model for a bank using a two hidden layer artificial neural networks .
    Obtained an accuracy of 86.25% over the 2000 examples of the test set data.
    Used Stochastic gradient descent with  with batch_size 10 and no of epochs  = 100 to train the nn model for 8000 examples.Adam optimization technique was also applied.

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/ann.py
@@ -1,3 +1,4 @@
+#importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,7 +7,7 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:,3:13].values
 y = dataset.iloc[:,13].values
 
-
+#data preprocessing
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:,1] = labelencoder_X_1.fit_transform(X[:,1])
@@ -26,3 +27,34 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Making the artificial neural network
+
+import keras 
+from keras.models import Sequential
+from keras.layers import Dense
+
+#initializing the neural network
+classifier = Sequential()
+
+#adding the input layer , hidden layers and output layers 
+classifier.add(Dense(output_dim =6,init = 'uniform',activation = 'relu',input_dim = 11))
+classifier.add(Dense(ouput_dim = 6,init = 'uniform',activation = 'relu'))
+classifier.add(Dense(output_dim = 1,init = 'uniform',activation = 'sigmoid'))
+
+#Applying stochastic gradient descent 
+
+classifier.compile(optimizer = 'adam',loss ='binary_crossentropy',metrics=['accuracy'])
+
+#Fitting the ANN
+classifier.fit(X_train,y_train,batch_size=10,nb_epoch = 100)
+
+#making the predictions and evaluating the model
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+
